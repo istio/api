6ed3168d9ae6c04359d389ec080cef0cb91ca3be
Kuat Yessenov <kuat@google.com>
2017-11-22 10:11:08 -0800

    add googleapis
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- scripts/generate-protos.sh
+++ scripts/generate-protos.sh
@@ -15,4 +15,15 @@ for src in ${files}; do
     fi
 done
 
+# Delete rewritten source
 rm mixer/v1/config/cfg.pb.go
+
+# Copy vendorized gogoprotos (not compatible with google.golang.org/api/)
+external=${genfiles}/external/com_github_googleapis_googleapis
+googleapis=$(find -L ${external} -name "*.pb.go")
+for src in ${googleapis}; do
+    dst=vendor/github.com/googleapis/googleapis/${src##${external}/}
+    if [ -d "$(dirname ${dst})" ]; then
+        install -m 0640 ${src} ${dst}
+    fi
+done
