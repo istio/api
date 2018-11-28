a453392c3f014eddf81ddd1f2132cf866c713317
Shriram Rajagopalan <shriramr@vmware.com>
2018-11-28 14:33:32 -0500

    nits and final updates
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>

--- networking/v1alpha3/network_scope.pb.go
+++ networking/v1alpha3/network_scope.pb.go
@@ -22,7 +22,10 @@ var _ = math.Inf
 type ConfigScope int32
 
 const (
-	ConfigScope_PUBLIC  ConfigScope = 0
+	// Config with this scope are visible to all workloads in the mesh
+	ConfigScope_PUBLIC ConfigScope = 0
+	// Configs with this scope are visible to only workloads in the same
+	// namespace as the configuration resource.
 	ConfigScope_PRIVATE ConfigScope = 1
 )
 
@@ -63,7 +66,7 @@ func (ConfigScope) EnumDescriptor() ([]byte, []int) { return fileDescriptorNetwo
 // compute the sidecar configuration for every workload in the namespace.
 //
 // NOTE 1: If workloads in the mesh depend only on other workloads in the
-// same namespace, set defaultNetworkScope.importMode to CURRENT_NAMESPACE
+// same namespace, set defaultNetworkScope.importMode to SAME_NAMESPACE
 // in the mesh global config map (in values.yaml).
 //
 // NOTE 2: To facilitate incremental pruning of the the sidecar
@@ -91,7 +94,7 @@ func (ConfigScope) EnumDescriptor() ([]byte, []int) { return fileDescriptorNetwo
 //     - namespace: prod-apis
 // ```
 //
-// In a mesh where the default network scope is set to CURRENT_NAMESPACE
+// In a mesh where the default network scope is set to SAME_NAMESPACE
 // only, if one or more workloads need to be able to reach every other
 // service in the mesh (e.g., metrics collection server), the following
 // NetworkScope resource can be used to specify such a dependency:
@@ -111,7 +114,7 @@ func (ConfigScope) EnumDescriptor() ([]byte, []int) { return fileDescriptorNetwo
 // The configuration above will allow workloads in the metrics-collection
 // namespace to access service in any namespace while workloads in other
 // namespaces will be configured for namespace local access as per the
-// global default network scope (CURRENT_NAMESPACE).
+// global default network scope (SAME_NAMESPACE).
 //
 type NetworkScope struct {
 	// REQUIRED. The set of services that workloads in this namespace are
@@ -185,6 +188,16 @@ func (m *NetworkScope_Import) GetHost() string {
 // Dependency describes a workload and the set of service dependencies
 // for the workload.
 type NetworkScope_Dependency struct {
+	// [#not-implemented-hide:]
+	// One or more labels that indicate a specific set of pods/VMs on which
+	// this dependency configuration should be applied.  The scope of label
+	// search is platform dependent.  On Kubernetes, for example, the scope
+	// includes pods running in the namespace in which the NetworkScope
+	// resource is present.  If the sourceWorkloadLabels are omitted, the
+	// imports specified will be applicable to all workloads in the current
+	// configuration namespace.
+	// NOTE: source_workload_labels are currently not supported.
+	SourceWorkloadLabels map[string]string `protobuf:"bytes,1,rep,name=source_workload_labels,json=sourceWorkloadLabels" json:"source_workload_labels,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
 	// REQUIRED: Import describes the set of namespaces whose exported
 	// services will be accessed by the workloads selected by the
 	// sourceWorkloadLabels. The sidecars attached to the workloads will be
@@ -201,6 +214,13 @@ func (m *NetworkScope_Dependency) String() string { return proto.CompactTextStri
 func (*NetworkScope_Dependency) ProtoMessage()    {}
 func (*NetworkScope_Dependency) Descriptor() ([]byte, []int) {
 	return fileDescriptorNetworkScope, []int{0, 1}
+}
+
+func (m *NetworkScope_Dependency) GetSourceWorkloadLabels() map[string]string {
+	if m != nil {
+		return m.SourceWorkloadLabels
+	}
+	return nil
 }
 
 func (m *NetworkScope_Dependency) GetImports() []*NetworkScope_Import {
@@ -291,6 +311,23 @@ func (m *NetworkScope_Dependency) MarshalTo(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.SourceWorkloadLabels) > 0 {
+		for k, _ := range m.SourceWorkloadLabels {
+			dAtA[i] = 0xa
+			i++
+			v := m.SourceWorkloadLabels[k]
+			mapSize := 1 + len(k) + sovNetworkScope(uint64(len(k))) + 1 + len(v) + sovNetworkScope(uint64(len(v)))
+			i = encodeVarintNetworkScope(dAtA, i, uint64(mapSize))
+			dAtA[i] = 0xa
+			i++
+			i = encodeVarintNetworkScope(dAtA, i, uint64(len(k)))
+			i += copy(dAtA[i:], k)
+			dAtA[i] = 0x12
+			i++
+			i = encodeVarintNetworkScope(dAtA, i, uint64(len(v)))
+			i += copy(dAtA[i:], v)
+		}
+	}
 	if len(m.Imports) > 0 {
 		for _, msg := range m.Imports {
 			dAtA[i] = 0x12
@@ -344,6 +381,14 @@ func (m *NetworkScope_Import) Size() (n int) {
 func (m *NetworkScope_Dependency) Size() (n int) {
 	var l int
 	_ = l
+	if len(m.SourceWorkloadLabels) > 0 {
+		for k, v := range m.SourceWorkloadLabels {
+			_ = k
+			_ = v
+			mapEntrySize := 1 + len(k) + sovNetworkScope(uint64(len(k))) + 1 + len(v) + sovNetworkScope(uint64(len(v)))
+			n += mapEntrySize + 1 + sovNetworkScope(uint64(mapEntrySize))
+		}
+	}
 	if len(m.Imports) > 0 {
 		for _, e := range m.Imports {
 			l = e.Size()
@@ -584,6 +629,124 @@ func (m *NetworkScope_Dependency) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Dependency: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field SourceWorkloadLabels", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowNetworkScope
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthNetworkScope
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.SourceWorkloadLabels == nil {
+				m.SourceWorkloadLabels = make(map[string]string)
+			}
+			var mapkey string
+			var mapvalue string
+			for iNdEx < postIndex {
+				entryPreIndex := iNdEx
+				var wire uint64
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return ErrIntOverflowNetworkScope
+					}
+					if iNdEx >= l {
+						return io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					wire |= (uint64(b) & 0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				fieldNum := int32(wire >> 3)
+				if fieldNum == 1 {
+					var stringLenmapkey uint64
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowNetworkScope
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						stringLenmapkey |= (uint64(b) & 0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+					intStringLenmapkey := int(stringLenmapkey)
+					if intStringLenmapkey < 0 {
+						return ErrInvalidLengthNetworkScope
+					}
+					postStringIndexmapkey := iNdEx + intStringLenmapkey
+					if postStringIndexmapkey > l {
+						return io.ErrUnexpectedEOF
+					}
+					mapkey = string(dAtA[iNdEx:postStringIndexmapkey])
+					iNdEx = postStringIndexmapkey
+				} else if fieldNum == 2 {
+					var stringLenmapvalue uint64
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowNetworkScope
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						stringLenmapvalue |= (uint64(b) & 0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+					intStringLenmapvalue := int(stringLenmapvalue)
+					if intStringLenmapvalue < 0 {
+						return ErrInvalidLengthNetworkScope
+					}
+					postStringIndexmapvalue := iNdEx + intStringLenmapvalue
+					if postStringIndexmapvalue > l {
+						return io.ErrUnexpectedEOF
+					}
+					mapvalue = string(dAtA[iNdEx:postStringIndexmapvalue])
+					iNdEx = postStringIndexmapvalue
+				} else {
+					iNdEx = entryPreIndex
+					skippy, err := skipNetworkScope(dAtA[iNdEx:])
+					if err != nil {
+						return err
+					}
+					if skippy < 0 {
+						return ErrInvalidLengthNetworkScope
+					}
+					if (iNdEx + skippy) > postIndex {
+						return io.ErrUnexpectedEOF
+					}
+					iNdEx += skippy
+				}
+			}
+			m.SourceWorkloadLabels[mapkey] = mapvalue
+			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Imports", wireType)
@@ -744,22 +907,27 @@ var (
 func init() { proto.RegisterFile("networking/v1alpha3/network_scope.proto", fileDescriptorNetworkScope) }
 
 var fileDescriptorNetworkScope = []byte{
-	// 266 bytes of a gzipped FileDescriptorProto
+	// 352 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0xcf, 0x4b, 0x2d, 0x29,
 	0xcf, 0x2f, 0xca, 0xce, 0xcc, 0x4b, 0xd7, 0x2f, 0x33, 0x4c, 0xcc, 0x29, 0xc8, 0x48, 0x34, 0xd6,
 	0x87, 0x8a, 0xc5, 0x17, 0x27, 0xe7, 0x17, 0xa4, 0xea, 0x15, 0x14, 0xe5, 0x97, 0xe4, 0x0b, 0x49,
-	0x66, 0x16, 0x97, 0x64, 0xe6, 0xeb, 0x21, 0x94, 0xeb, 0xc1, 0x94, 0x2b, 0xfd, 0x62, 0xe4, 0xe2,
+	0x66, 0x16, 0x97, 0x64, 0xe6, 0xeb, 0x21, 0x94, 0xeb, 0xc1, 0x94, 0x2b, 0x9d, 0x64, 0xe6, 0xe2,
 	0xf1, 0x83, 0x88, 0x07, 0x83, 0x74, 0x08, 0x85, 0x71, 0xf1, 0xa4, 0xa4, 0x16, 0xa4, 0xe6, 0xa5,
 	0xa4, 0xe6, 0x25, 0x67, 0xa6, 0x16, 0x4b, 0x30, 0x2a, 0x30, 0x6b, 0x70, 0x1b, 0x19, 0xe9, 0xe1,
 	0x34, 0x42, 0x0f, 0x59, 0xbb, 0x9e, 0x0b, 0x4c, 0x6f, 0x65, 0x10, 0x8a, 0x39, 0x52, 0x56, 0x5c,
 	0x6c, 0x9e, 0xb9, 0x05, 0xf9, 0x45, 0x25, 0x42, 0x32, 0x5c, 0x9c, 0x79, 0x89, 0xb9, 0xa9, 0xc5,
 	0x05, 0x89, 0xc9, 0xa9, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x08, 0x01, 0x21, 0x21, 0x2e,
-	0x96, 0x8c, 0xfc, 0xe2, 0x12, 0x09, 0x26, 0xb0, 0x04, 0x98, 0x2d, 0x15, 0xc6, 0xc5, 0x85, 0x30,
-	0x57, 0xc8, 0x83, 0x8b, 0x3d, 0x13, 0x6c, 0x52, 0xb1, 0x04, 0x13, 0xd8, 0x71, 0x7a, 0xc4, 0x3a,
-	0x0e, 0xe2, 0x80, 0x20, 0x98, 0x76, 0x2d, 0x35, 0x2e, 0x6e, 0xe7, 0xfc, 0xbc, 0xb4, 0xcc, 0x74,
-	0x88, 0xd7, 0xb9, 0xb8, 0xd8, 0x02, 0x42, 0x9d, 0x7c, 0x3c, 0x9d, 0x05, 0x18, 0x84, 0xb8, 0xb9,
-	0xd8, 0x03, 0x82, 0x3c, 0xc3, 0x1c, 0x43, 0x5c, 0x05, 0x18, 0x9d, 0xf4, 0x4e, 0x3c, 0x92, 0x63,
-	0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x28, 0x05, 0x88, 0x6d, 0x99, 0xf9, 0xfa,
-	0x89, 0x05, 0x99, 0xfa, 0x58, 0xe2, 0x20, 0x89, 0x0d, 0x1c, 0xec, 0xc6, 0x80, 0x00, 0x00, 0x00,
-	0xff, 0xff, 0x7a, 0xa8, 0x8e, 0x07, 0xa1, 0x01, 0x00, 0x00,
+	0x96, 0x8c, 0xfc, 0xe2, 0x12, 0x09, 0x26, 0xb0, 0x04, 0x98, 0x2d, 0xb5, 0x84, 0x89, 0x8b, 0x0b,
+	0x61, 0xb0, 0x50, 0x13, 0x23, 0x97, 0x58, 0x71, 0x7e, 0x69, 0x51, 0x72, 0x6a, 0x3c, 0xc8, 0xe2,
+	0x9c, 0xfc, 0xc4, 0x94, 0xf8, 0x9c, 0xc4, 0xa4, 0xd4, 0x1c, 0x98, 0x6b, 0x7d, 0x48, 0x77, 0xad,
+	0x5e, 0x30, 0xd8, 0xc0, 0x70, 0xa8, 0x79, 0x3e, 0x60, 0xe3, 0x5c, 0xf3, 0x4a, 0x8a, 0x2a, 0x83,
+	0x44, 0x8a, 0xb1, 0x48, 0x09, 0x79, 0x70, 0xb1, 0x67, 0x82, 0xfd, 0x53, 0x2c, 0xc1, 0x04, 0xb6,
+	0x54, 0x8f, 0x58, 0x4b, 0x21, 0xc1, 0x10, 0x04, 0xd3, 0x2e, 0xe5, 0xce, 0x25, 0x89, 0xd3, 0x72,
+	0x21, 0x01, 0x2e, 0xe6, 0xec, 0xd4, 0x4a, 0x68, 0x30, 0x81, 0x98, 0x42, 0x22, 0x5c, 0xac, 0x65,
+	0x89, 0x39, 0xa5, 0xa9, 0xd0, 0x10, 0x82, 0x70, 0xac, 0x98, 0x2c, 0x18, 0xb5, 0xd4, 0xb8, 0xb8,
+	0x9d, 0xf3, 0xf3, 0xd2, 0x32, 0xd3, 0x21, 0x31, 0xc9, 0xc5, 0xc5, 0x16, 0x10, 0xea, 0xe4, 0xe3,
+	0xe9, 0x2c, 0xc0, 0x20, 0xc4, 0xcd, 0xc5, 0x1e, 0x10, 0xe4, 0x19, 0xe6, 0x18, 0xe2, 0x2a, 0xc0,
+	0xe8, 0xa4, 0x77, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x46,
+	0x29, 0x40, 0x9c, 0x9d, 0x99, 0xaf, 0x9f, 0x58, 0x90, 0xa9, 0x8f, 0x25, 0x49, 0x25, 0xb1, 0x81,
+	0x53, 0x91, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x64, 0xd5, 0x9b, 0x1f, 0x70, 0x02, 0x00, 0x00,
 }
