671193cfe9be752487dacf19029168da2b8a3a1a
Shriram Rajagopalan <shriramr@vmware.com>
2019-01-04 20:54:12 -0500

    capture mode
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>

--- python/istio_api/networking/v1alpha3/sidecar_pb2.py
+++ python/istio_api/networking/v1alpha3/sidecar_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='networking/v1alpha3/sidecar.proto',
   package='istio.networking.v1alpha3',
   syntax='proto3',
-  serialized_pb=_b('\n!networking/v1alpha3/sidecar.proto\x12\x19istio.networking.v1alpha3\x1a!networking/v1alpha3/gateway.proto\"\xc6\x01\n\x07Sidecar\x12\x46\n\x11workload_selector\x18\x01 \x01(\x0b\x32+.istio.networking.v1alpha3.WorkloadSelector\x12\x39\n\x07ingress\x18\x02 \x03(\x0b\x32(.istio.networking.v1alpha3.IstioListener\x12\x38\n\x06\x65gress\x18\x03 \x03(\x0b\x32(.istio.networking.v1alpha3.IstioListener\"\x99\x01\n\rIstioListener\x12\x0c\n\x04name\x18\x01 \x01(\t\x12-\n\x04port\x18\x02 \x01(\x0b\x32\x1f.istio.networking.v1alpha3.Port\x12\x0c\n\x04\x62ind\x18\x03 \x01(\t\x12\x14\n\x0c\x62ind_to_port\x18\x04 \x01(\x08\x12\r\n\x05hosts\x18\x05 \x03(\t\x12\x18\n\x10\x64\x65\x66\x61ult_endpoint\x18\x06 \x01(\t\"\x8a\x01\n\x10WorkloadSelector\x12G\n\x06labels\x18\x01 \x03(\x0b\x32\x37.istio.networking.v1alpha3.WorkloadSelector.LabelsEntry\x1a-\n\x0bLabelsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01*&\n\x0b\x43onfigScope\x12\n\n\x06PUBLIC\x10\x00\x12\x0b\n\x07PRIVATE\x10\x01\x42\"Z istio.io/api/networking/v1alpha3b\x06proto3')
+  serialized_pb=_b('\n!networking/v1alpha3/sidecar.proto\x12\x19istio.networking.v1alpha3\x1a!networking/v1alpha3/gateway.proto\"\xc6\x01\n\x07Sidecar\x12\x46\n\x11workload_selector\x18\x01 \x01(\x0b\x32+.istio.networking.v1alpha3.WorkloadSelector\x12\x39\n\x07ingress\x18\x02 \x03(\x0b\x32(.istio.networking.v1alpha3.IstioListener\x12\x38\n\x06\x65gress\x18\x03 \x03(\x0b\x32(.istio.networking.v1alpha3.IstioListener\"\x83\x02\n\rIstioListener\x12\x0c\n\x04name\x18\x01 \x01(\t\x12-\n\x04port\x18\x02 \x01(\x0b\x32\x1f.istio.networking.v1alpha3.Port\x12\x0c\n\x04\x62ind\x18\x03 \x01(\t\x12J\n\x0c\x63\x61pture_mode\x18\x04 \x01(\x0e\x32\x34.istio.networking.v1alpha3.IstioListener.CaptureMode\x12\r\n\x05hosts\x18\x05 \x03(\t\x12\x18\n\x10\x64\x65\x66\x61ult_endpoint\x18\x06 \x01(\t\"2\n\x0b\x43\x61ptureMode\x12\x0b\n\x07\x44\x45\x46\x41ULT\x10\x00\x12\x0c\n\x08IPTABLES\x10\x01\x12\x08\n\x04NONE\x10\x02\"\x8a\x01\n\x10WorkloadSelector\x12G\n\x06labels\x18\x01 \x03(\x0b\x32\x37.istio.networking.v1alpha3.WorkloadSelector.LabelsEntry\x1a-\n\x0bLabelsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01*&\n\x0b\x43onfigScope\x12\n\n\x06PUBLIC\x10\x00\x12\x0b\n\x07PRIVATE\x10\x01\x42\"Z istio.io/api/networking/v1alpha3b\x06proto3')
   ,
   dependencies=[networking_dot_v1alpha3_dot_gateway__pb2.DESCRIPTOR,])
 
@@ -42,8 +42,8 @@ _CONFIGSCOPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=597,
-  serialized_end=635,
+  serialized_start=703,
+  serialized_end=741,
 )
 _sym_db.RegisterEnumDescriptor(_CONFIGSCOPE)
 
@@ -51,6 +51,32 @@ ConfigScope = enum_type_wrapper.EnumTypeWrapper(_CONFIGSCOPE)
 PUBLIC = 0
 PRIVATE = 1
 
+
+_ISTIOLISTENER_CAPTUREMODE = _descriptor.EnumDescriptor(
+  name='CaptureMode',
+  full_name='istio.networking.v1alpha3.IstioListener.CaptureMode',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='DEFAULT', index=0, number=0,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='IPTABLES', index=1, number=1,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='NONE', index=2, number=2,
+      options=None,
+      type=None),
+  ],
+  containing_type=None,
+  options=None,
+  serialized_start=510,
+  serialized_end=560,
+)
+_sym_db.RegisterEnumDescriptor(_ISTIOLISTENER_CAPTUREMODE)
 
 
 _SIDECAR = _descriptor.Descriptor(
@@ -127,9 +153,9 @@ _ISTIOLISTENER = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='bind_to_port', full_name='istio.networking.v1alpha3.IstioListener.bind_to_port', index=3,
-      number=4, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
+      name='capture_mode', full_name='istio.networking.v1alpha3.IstioListener.capture_mode', index=3,
+      number=4, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
@@ -152,6 +178,7 @@ _ISTIOLISTENER = _descriptor.Descriptor(
   ],
   nested_types=[],
   enum_types=[
+    _ISTIOLISTENER_CAPTUREMODE,
   ],
   options=None,
   is_extendable=False,
@@ -160,7 +187,7 @@ _ISTIOLISTENER = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=301,
-  serialized_end=454,
+  serialized_end=560,
 )
 
 
@@ -197,8 +224,8 @@ _WORKLOADSELECTOR_LABELSENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=550,
-  serialized_end=595,
+  serialized_start=656,
+  serialized_end=701,
 )
 
 _WORKLOADSELECTOR = _descriptor.Descriptor(
@@ -227,14 +254,16 @@ _WORKLOADSELECTOR = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=457,
-  serialized_end=595,
+  serialized_start=563,
+  serialized_end=701,
 )
 
 _SIDECAR.fields_by_name['workload_selector'].message_type = _WORKLOADSELECTOR
 _SIDECAR.fields_by_name['ingress'].message_type = _ISTIOLISTENER
 _SIDECAR.fields_by_name['egress'].message_type = _ISTIOLISTENER
 _ISTIOLISTENER.fields_by_name['port'].message_type = networking_dot_v1alpha3_dot_gateway__pb2._PORT
+_ISTIOLISTENER.fields_by_name['capture_mode'].enum_type = _ISTIOLISTENER_CAPTUREMODE
+_ISTIOLISTENER_CAPTUREMODE.containing_type = _ISTIOLISTENER
 _WORKLOADSELECTOR_LABELSENTRY.containing_type = _WORKLOADSELECTOR
 _WORKLOADSELECTOR.fields_by_name['labels'].message_type = _WORKLOADSELECTOR_LABELSENTRY
 DESCRIPTOR.message_types_by_name['Sidecar'] = _SIDECAR
