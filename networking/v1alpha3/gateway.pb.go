609fbd324ba7a2250e46fc61d3832fc861e7ebd4
Ed Snible <esnible@acm.org>
2019-03-22 06:42:40 -0700

    Be precise about 'workload' vs 'workload instance' (#876)
    (cherry picked from commit 785ea83cee806d5a68ceb25a46fde0f65e0b56c9)

--- networking/v1alpha3/gateway.pb.go
+++ networking/v1alpha3/gateway.pb.go
@@ -284,11 +284,11 @@ func (Server_TLSOptions_TLSProtocol) EnumDescriptor() ([]byte, []int) {
 type Gateway struct {
 	// REQUIRED: A list of server specifications.
 	Servers []*Server `protobuf:"bytes,1,rep,name=servers,proto3" json:"servers,omitempty"`
-	// REQUIRED: One or more labels used to select the specific gateway workload
-	// to which this configuration should be applied.
-	// It is recommended that the Gateway resource reside in the same namespace
-	// as the gateway workload.
-	// This may become a requirement in the future.
+	// REQUIRED: One or more labels that indicate a specific set of pods/VMs
+	// on which this gateway configuration should be applied. The scope of
+	// label search is restricted to the configuration namespace in which the
+	// the resource is present. In other words, the Gateway resource must
+	// reside in the same namespace as the gateway workload instance.
 	Selector             map[string]string `protobuf:"bytes,2,rep,name=selector,proto3" json:"selector,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
 	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
 	XXX_unrecognized     []byte            `json:"-"`
@@ -545,7 +545,7 @@ type Server_TLSOptions struct {
 	// as Kubernetes secrets, will be configured to retrieve the
 	// serverCertificate and the privateKey using credentialName, instead
 	// of using the file system paths specified above. If using mutual TLS,
-	// gateway workloads will retrieve the CaCertificates using
+	// gateway workload instances will retrieve the CaCertificates using
 	// credentialName-cacert. The semantics of the name are platform
 	// dependent.  In Kubernetes, the default Istio supplied credential
 	// server expects the credentialName to match the name of the
