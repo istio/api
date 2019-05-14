6a4444481148a70c0fd17ff9f0f07ced27555b00
gargnupur <gargnupur@google.com>
2019-05-13 17:02:17 -0700

    Add a status field in QuotaResult
    This is so that we can result status when we are not able to fetch quota and check call can fail based on fail-open policy set by user.
    Issue: https://github.com/istio/istio/issues/12115

--- python/istio_api/mixer/adapter/model/v1beta1/quota_pb2.py
+++ python/istio_api/mixer/adapter/model/v1beta1/quota_pb2.py
@@ -15,15 +15,16 @@ _sym_db = _symbol_database.Default()
 
 from gogoproto import gogo_pb2 as gogoproto_dot_gogo__pb2
 from google.protobuf import duration_pb2 as google_dot_protobuf_dot_duration__pb2
+from google.rpc import status_pb2 as google_dot_rpc_dot_status__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='mixer/adapter/model/v1beta1/quota.proto',
   package='istio.mixer.adapter.model.v1beta1',
   syntax='proto3',
-  serialized_pb=_b('\n\'mixer/adapter/model/v1beta1/quota.proto\x12!istio.mixer.adapter.model.v1beta1\x1a\x14gogoproto/gogo.proto\x1a\x1egoogle/protobuf/duration.proto\"\x81\x02\n\x0cQuotaRequest\x12Q\n\x06quotas\x18\x01 \x03(\x0b\x32;.istio.mixer.adapter.model.v1beta1.QuotaRequest.QuotasEntryB\x04\xc8\xde\x1f\x00\x1a\x32\n\x0bQuotaParams\x12\x0e\n\x06\x61mount\x18\x01 \x01(\x03\x12\x13\n\x0b\x62\x65st_effort\x18\x02 \x01(\x08\x1aj\n\x0bQuotasEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12J\n\x05value\x18\x02 \x01(\x0b\x32;.istio.mixer.adapter.model.v1beta1.QuotaRequest.QuotaParams:\x02\x38\x01\"\xa4\x02\n\x0bQuotaResult\x12P\n\x06quotas\x18\x01 \x03(\x0b\x32:.istio.mixer.adapter.model.v1beta1.QuotaResult.QuotasEntryB\x04\xc8\xde\x1f\x00\x1a]\n\x06Result\x12;\n\x0evalid_duration\x18\x02 \x01(\x0b\x32\x19.google.protobuf.DurationB\x08\xc8\xde\x1f\x00\x98\xdf\x1f\x01\x12\x16\n\x0egranted_amount\x18\x03 \x01(\x03\x1a\x64\n\x0bQuotasEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x44\n\x05value\x18\x02 \x01(\x0b\x32\x35.istio.mixer.adapter.model.v1beta1.QuotaResult.Result:\x02\x38\x01\x42\x36Z(istio.io/api/mixer/adapter/model/v1beta1\xc8\xe1\x1e\x00\xa8\xe2\x1e\x00\xf0\xe1\x1e\x00\x62\x06proto3')
+  serialized_pb=_b('\n\'mixer/adapter/model/v1beta1/quota.proto\x12!istio.mixer.adapter.model.v1beta1\x1a\x14gogoproto/gogo.proto\x1a\x1egoogle/protobuf/duration.proto\x1a\x17google/rpc/status.proto\"\x81\x02\n\x0cQuotaRequest\x12Q\n\x06quotas\x18\x01 \x03(\x0b\x32;.istio.mixer.adapter.model.v1beta1.QuotaRequest.QuotasEntryB\x04\xc8\xde\x1f\x00\x1a\x32\n\x0bQuotaParams\x12\x0e\n\x06\x61mount\x18\x01 \x01(\x03\x12\x13\n\x0b\x62\x65st_effort\x18\x02 \x01(\x08\x1aj\n\x0bQuotasEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12J\n\x05value\x18\x02 \x01(\x0b\x32;.istio.mixer.adapter.model.v1beta1.QuotaRequest.QuotaParams:\x02\x38\x01\"\xcf\x02\n\x0bQuotaResult\x12P\n\x06quotas\x18\x01 \x03(\x0b\x32:.istio.mixer.adapter.model.v1beta1.QuotaResult.QuotasEntryB\x04\xc8\xde\x1f\x00\x1a\x87\x01\n\x06Result\x12;\n\x0evalid_duration\x18\x02 \x01(\x0b\x32\x19.google.protobuf.DurationB\x08\xc8\xde\x1f\x00\x98\xdf\x1f\x01\x12\x16\n\x0egranted_amount\x18\x03 \x01(\x03\x12(\n\x06status\x18\x04 \x01(\x0b\x32\x12.google.rpc.StatusB\x04\xc8\xde\x1f\x00\x1a\x64\n\x0bQuotasEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x44\n\x05value\x18\x02 \x01(\x0b\x32\x35.istio.mixer.adapter.model.v1beta1.QuotaResult.Result:\x02\x38\x01\x42\x36Z(istio.io/api/mixer/adapter/model/v1beta1\xc8\xe1\x1e\x00\xa8\xe2\x1e\x00\xf0\xe1\x1e\x00\x62\x06proto3')
   ,
-  dependencies=[gogoproto_dot_gogo__pb2.DESCRIPTOR,google_dot_protobuf_dot_duration__pb2.DESCRIPTOR,])
+  dependencies=[gogoproto_dot_gogo__pb2.DESCRIPTOR,google_dot_protobuf_dot_duration__pb2.DESCRIPTOR,google_dot_rpc_dot_status__pb2.DESCRIPTOR,])
 
 
 
@@ -61,8 +62,8 @@ _QUOTAREQUEST_QUOTAPARAMS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=232,
-  serialized_end=282,
+  serialized_start=257,
+  serialized_end=307,
 )
 
 _QUOTAREQUEST_QUOTASENTRY = _descriptor.Descriptor(
@@ -98,8 +99,8 @@ _QUOTAREQUEST_QUOTASENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=284,
-  serialized_end=390,
+  serialized_start=309,
+  serialized_end=415,
 )
 
 _QUOTAREQUEST = _descriptor.Descriptor(
@@ -128,8 +129,8 @@ _QUOTAREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=133,
-  serialized_end=390,
+  serialized_start=158,
+  serialized_end=415,
 )
 
 
@@ -154,6 +155,13 @@ _QUOTARESULT_RESULT = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='status', full_name='istio.mixer.adapter.model.v1beta1.QuotaResult.Result.status', index=2,
+      number=4, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=_descriptor._ParseOptions(descriptor_pb2.FieldOptions(), _b('\310\336\037\000')), file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -166,8 +174,8 @@ _QUOTARESULT_RESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=490,
-  serialized_end=583,
+  serialized_start=516,
+  serialized_end=651,
 )
 
 _QUOTARESULT_QUOTASENTRY = _descriptor.Descriptor(
@@ -203,8 +211,8 @@ _QUOTARESULT_QUOTASENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=585,
-  serialized_end=685,
+  serialized_start=653,
+  serialized_end=753,
 )
 
 _QUOTARESULT = _descriptor.Descriptor(
@@ -233,8 +241,8 @@ _QUOTARESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=393,
-  serialized_end=685,
+  serialized_start=418,
+  serialized_end=753,
 )
 
 _QUOTAREQUEST_QUOTAPARAMS.containing_type = _QUOTAREQUEST
@@ -242,6 +250,7 @@ _QUOTAREQUEST_QUOTASENTRY.fields_by_name['value'].message_type = _QUOTAREQUEST_Q
 _QUOTAREQUEST_QUOTASENTRY.containing_type = _QUOTAREQUEST
 _QUOTAREQUEST.fields_by_name['quotas'].message_type = _QUOTAREQUEST_QUOTASENTRY
 _QUOTARESULT_RESULT.fields_by_name['valid_duration'].message_type = google_dot_protobuf_dot_duration__pb2._DURATION
+_QUOTARESULT_RESULT.fields_by_name['status'].message_type = google_dot_rpc_dot_status__pb2._STATUS
 _QUOTARESULT_RESULT.containing_type = _QUOTARESULT
 _QUOTARESULT_QUOTASENTRY.fields_by_name['value'].message_type = _QUOTARESULT_RESULT
 _QUOTARESULT_QUOTASENTRY.containing_type = _QUOTARESULT
@@ -305,6 +314,8 @@ _QUOTAREQUEST.fields_by_name['quotas'].has_options = True
 _QUOTAREQUEST.fields_by_name['quotas']._options = _descriptor._ParseOptions(descriptor_pb2.FieldOptions(), _b('\310\336\037\000'))
 _QUOTARESULT_RESULT.fields_by_name['valid_duration'].has_options = True
 _QUOTARESULT_RESULT.fields_by_name['valid_duration']._options = _descriptor._ParseOptions(descriptor_pb2.FieldOptions(), _b('\310\336\037\000\230\337\037\001'))
+_QUOTARESULT_RESULT.fields_by_name['status'].has_options = True
+_QUOTARESULT_RESULT.fields_by_name['status']._options = _descriptor._ParseOptions(descriptor_pb2.FieldOptions(), _b('\310\336\037\000'))
 _QUOTARESULT_QUOTASENTRY.has_options = True
 _QUOTARESULT_QUOTASENTRY._options = _descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001'))
 _QUOTARESULT.fields_by_name['quotas'].has_options = True
