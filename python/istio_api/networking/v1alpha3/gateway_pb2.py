5ab7e535476e58502cb8caf3533865c84e7afde3
Phlak106 <cws4173@outlook.com>
2019-07-24 21:08:38 +0000

    Remove trusted_ca field and CertificateValidationContext message. Move validate_certificate_spki near other validation settings. Add validate_certificate_hash.

--- python/istio_api/networking/v1alpha3/gateway_pb2.py
+++ python/istio_api/networking/v1alpha3/gateway_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='networking/v1alpha3/gateway.proto',
   package='istio.networking.v1alpha3',
   syntax='proto3',
-  serialized_pb=_b('\n!networking/v1alpha3/gateway.proto\x12\x19istio.networking.v1alpha3\"\xb2\x01\n\x07Gateway\x12\x32\n\x07servers\x18\x01 \x03(\x0b\x32!.istio.networking.v1alpha3.Server\x12\x42\n\x08selector\x18\x02 \x03(\x0b\x32\x30.istio.networking.v1alpha3.Gateway.SelectorEntry\x1a/\n\rSelectorEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\xc4\x07\n\x06Server\x12-\n\x04port\x18\x01 \x01(\x0b\x32\x1f.istio.networking.v1alpha3.Port\x12\x0c\n\x04\x62ind\x18\x04 \x01(\t\x12\r\n\x05hosts\x18\x02 \x03(\t\x12\x39\n\x03tls\x18\x03 \x01(\x0b\x32,.istio.networking.v1alpha3.Server.TLSOptions\x12\x18\n\x10\x64\x65\x66\x61ult_endpoint\x18\x05 \x01(\t\x1a\x98\x06\n\nTLSOptions\x12\x16\n\x0ehttps_redirect\x18\x01 \x01(\x08\x12\x42\n\x04mode\x18\x02 \x01(\x0e\x32\x34.istio.networking.v1alpha3.Server.TLSOptions.TLSmode\x12\x1a\n\x12server_certificate\x18\x03 \x01(\t\x12\x13\n\x0bprivate_key\x18\x04 \x01(\t\x12\x17\n\x0f\x63\x61_certificates\x18\x05 \x01(\t\x12\x17\n\x0f\x63redential_name\x18\n \x01(\t\x12\x19\n\x11subject_alt_names\x18\x06 \x03(\t\x12V\n\x14min_protocol_version\x18\x07 \x01(\x0e\x32\x38.istio.networking.v1alpha3.Server.TLSOptions.TLSProtocol\x12V\n\x14max_protocol_version\x18\x08 \x01(\x0e\x32\x38.istio.networking.v1alpha3.Server.TLSOptions.TLSProtocol\x12\x15\n\rcipher_suites\x18\t \x03(\t\x12y\n&default_certificate_validation_context\x18\x0b \x01(\x0b\x32I.istio.networking.v1alpha3.Server.TLSOptions.CertificateValidationContext\x1aS\n\x1c\x43\x65rtificateValidationContext\x12\x12\n\ntrusted_ca\x18\x01 \x01(\t\x12\x1f\n\x17verify_certificate_spki\x18\x02 \x03(\t\"H\n\x07TLSmode\x12\x0f\n\x0bPASSTHROUGH\x10\x00\x12\n\n\x06SIMPLE\x10\x01\x12\n\n\x06MUTUAL\x10\x02\x12\x14\n\x10\x41UTO_PASSTHROUGH\x10\x03\"O\n\x0bTLSProtocol\x12\x0c\n\x08TLS_AUTO\x10\x00\x12\x0b\n\x07TLSV1_0\x10\x01\x12\x0b\n\x07TLSV1_1\x10\x02\x12\x0b\n\x07TLSV1_2\x10\x03\x12\x0b\n\x07TLSV1_3\x10\x04\"6\n\x04Port\x12\x0e\n\x06number\x18\x01 \x01(\r\x12\x10\n\x08protocol\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\tB\"Z istio.io/api/networking/v1alpha3b\x06proto3')
+  serialized_pb=_b('\n!networking/v1alpha3/gateway.proto\x12\x19istio.networking.v1alpha3\"\xb2\x01\n\x07Gateway\x12\x32\n\x07servers\x18\x01 \x03(\x0b\x32!.istio.networking.v1alpha3.Server\x12\x42\n\x08selector\x18\x02 \x03(\x0b\x32\x30.istio.networking.v1alpha3.Gateway.SelectorEntry\x1a/\n\rSelectorEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\xb6\x06\n\x06Server\x12-\n\x04port\x18\x01 \x01(\x0b\x32\x1f.istio.networking.v1alpha3.Port\x12\x0c\n\x04\x62ind\x18\x04 \x01(\t\x12\r\n\x05hosts\x18\x02 \x03(\t\x12\x39\n\x03tls\x18\x03 \x01(\x0b\x32,.istio.networking.v1alpha3.Server.TLSOptions\x12\x18\n\x10\x64\x65\x66\x61ult_endpoint\x18\x05 \x01(\t\x1a\x8a\x05\n\nTLSOptions\x12\x16\n\x0ehttps_redirect\x18\x01 \x01(\x08\x12\x42\n\x04mode\x18\x02 \x01(\x0e\x32\x34.istio.networking.v1alpha3.Server.TLSOptions.TLSmode\x12\x1a\n\x12server_certificate\x18\x03 \x01(\t\x12\x13\n\x0bprivate_key\x18\x04 \x01(\t\x12\x17\n\x0f\x63\x61_certificates\x18\x05 \x01(\t\x12\x17\n\x0f\x63redential_name\x18\n \x01(\t\x12\x19\n\x11subject_alt_names\x18\x06 \x03(\t\x12\x1f\n\x17verify_certificate_spki\x18\x0b \x03(\t\x12\x1f\n\x17verify_certificate_hash\x18\x0c \x03(\t\x12V\n\x14min_protocol_version\x18\x07 \x01(\x0e\x32\x38.istio.networking.v1alpha3.Server.TLSOptions.TLSProtocol\x12V\n\x14max_protocol_version\x18\x08 \x01(\x0e\x32\x38.istio.networking.v1alpha3.Server.TLSOptions.TLSProtocol\x12\x15\n\rcipher_suites\x18\t \x03(\t\"H\n\x07TLSmode\x12\x0f\n\x0bPASSTHROUGH\x10\x00\x12\n\n\x06SIMPLE\x10\x01\x12\n\n\x06MUTUAL\x10\x02\x12\x14\n\x10\x41UTO_PASSTHROUGH\x10\x03\"O\n\x0bTLSProtocol\x12\x0c\n\x08TLS_AUTO\x10\x00\x12\x0b\n\x07TLSV1_0\x10\x01\x12\x0b\n\x07TLSV1_1\x10\x02\x12\x0b\n\x07TLSV1_2\x10\x03\x12\x0b\n\x07TLSV1_3\x10\x04\"6\n\x04Port\x12\x0e\n\x06number\x18\x01 \x01(\r\x12\x10\n\x08protocol\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\tB\"Z istio.io/api/networking/v1alpha3b\x06proto3')
 )
 
 
@@ -49,8 +49,8 @@ _SERVER_TLSOPTIONS_TLSMODE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=1057,
-  serialized_end=1129,
+  serialized_start=915,
+  serialized_end=987,
 )
 _sym_db.RegisterEnumDescriptor(_SERVER_TLSOPTIONS_TLSMODE)
 
@@ -83,8 +83,8 @@ _SERVER_TLSOPTIONS_TLSPROTOCOL = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=1131,
-  serialized_end=1210,
+  serialized_start=989,
+  serialized_end=1068,
 )
 _sym_db.RegisterEnumDescriptor(_SERVER_TLSOPTIONS_TLSPROTOCOL)
 
@@ -164,43 +164,6 @@ _GATEWAY = _descriptor.Descriptor(
 )
 
 
-_SERVER_TLSOPTIONS_CERTIFICATEVALIDATIONCONTEXT = _descriptor.Descriptor(
-  name='CertificateValidationContext',
-  full_name='istio.networking.v1alpha3.Server.TLSOptions.CertificateValidationContext',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='trusted_ca', full_name='istio.networking.v1alpha3.Server.TLSOptions.CertificateValidationContext.trusted_ca', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='verify_certificate_spki', full_name='istio.networking.v1alpha3.Server.TLSOptions.CertificateValidationContext.verify_certificate_spki', index=1,
-      number=2, type=9, cpp_type=9, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=972,
-  serialized_end=1055,
-)
-
 _SERVER_TLSOPTIONS = _descriptor.Descriptor(
   name='TLSOptions',
   full_name='istio.networking.v1alpha3.Server.TLSOptions',
@@ -258,37 +221,44 @@ _SERVER_TLSOPTIONS = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='min_protocol_version', full_name='istio.networking.v1alpha3.Server.TLSOptions.min_protocol_version', index=7,
+      name='verify_certificate_spki', full_name='istio.networking.v1alpha3.Server.TLSOptions.verify_certificate_spki', index=7,
+      number=11, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='verify_certificate_hash', full_name='istio.networking.v1alpha3.Server.TLSOptions.verify_certificate_hash', index=8,
+      number=12, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='min_protocol_version', full_name='istio.networking.v1alpha3.Server.TLSOptions.min_protocol_version', index=9,
       number=7, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='max_protocol_version', full_name='istio.networking.v1alpha3.Server.TLSOptions.max_protocol_version', index=8,
+      name='max_protocol_version', full_name='istio.networking.v1alpha3.Server.TLSOptions.max_protocol_version', index=10,
       number=8, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='cipher_suites', full_name='istio.networking.v1alpha3.Server.TLSOptions.cipher_suites', index=9,
+      name='cipher_suites', full_name='istio.networking.v1alpha3.Server.TLSOptions.cipher_suites', index=11,
       number=9, type=9, cpp_type=9, label=3,
       has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='default_certificate_validation_context', full_name='istio.networking.v1alpha3.Server.TLSOptions.default_certificate_validation_context', index=10,
-      number=11, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
-  nested_types=[_SERVER_TLSOPTIONS_CERTIFICATEVALIDATIONCONTEXT, ],
+  nested_types=[],
   enum_types=[
     _SERVER_TLSOPTIONS_TLSMODE,
     _SERVER_TLSOPTIONS_TLSPROTOCOL,
@@ -300,7 +270,7 @@ _SERVER_TLSOPTIONS = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=418,
-  serialized_end=1210,
+  serialized_end=1068,
 )
 
 _SERVER = _descriptor.Descriptor(
@@ -358,7 +328,7 @@ _SERVER = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=246,
-  serialized_end=1210,
+  serialized_end=1068,
 )
 
 
@@ -402,18 +372,16 @@ _PORT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1212,
-  serialized_end=1266,
+  serialized_start=1070,
+  serialized_end=1124,
 )
 
 _GATEWAY_SELECTORENTRY.containing_type = _GATEWAY
 _GATEWAY.fields_by_name['servers'].message_type = _SERVER
 _GATEWAY.fields_by_name['selector'].message_type = _GATEWAY_SELECTORENTRY
-_SERVER_TLSOPTIONS_CERTIFICATEVALIDATIONCONTEXT.containing_type = _SERVER_TLSOPTIONS
 _SERVER_TLSOPTIONS.fields_by_name['mode'].enum_type = _SERVER_TLSOPTIONS_TLSMODE
 _SERVER_TLSOPTIONS.fields_by_name['min_protocol_version'].enum_type = _SERVER_TLSOPTIONS_TLSPROTOCOL
 _SERVER_TLSOPTIONS.fields_by_name['max_protocol_version'].enum_type = _SERVER_TLSOPTIONS_TLSPROTOCOL
-_SERVER_TLSOPTIONS.fields_by_name['default_certificate_validation_context'].message_type = _SERVER_TLSOPTIONS_CERTIFICATEVALIDATIONCONTEXT
 _SERVER_TLSOPTIONS.containing_type = _SERVER
 _SERVER_TLSOPTIONS_TLSMODE.containing_type = _SERVER_TLSOPTIONS
 _SERVER_TLSOPTIONS_TLSPROTOCOL.containing_type = _SERVER_TLSOPTIONS
@@ -442,13 +410,6 @@ _sym_db.RegisterMessage(Gateway.SelectorEntry)
 Server = _reflection.GeneratedProtocolMessageType('Server', (_message.Message,), dict(
 
   TLSOptions = _reflection.GeneratedProtocolMessageType('TLSOptions', (_message.Message,), dict(
-
-    CertificateValidationContext = _reflection.GeneratedProtocolMessageType('CertificateValidationContext', (_message.Message,), dict(
-      DESCRIPTOR = _SERVER_TLSOPTIONS_CERTIFICATEVALIDATIONCONTEXT,
-      __module__ = 'networking.v1alpha3.gateway_pb2'
-      # @@protoc_insertion_point(class_scope:istio.networking.v1alpha3.Server.TLSOptions.CertificateValidationContext)
-      ))
-    ,
     DESCRIPTOR = _SERVER_TLSOPTIONS,
     __module__ = 'networking.v1alpha3.gateway_pb2'
     # @@protoc_insertion_point(class_scope:istio.networking.v1alpha3.Server.TLSOptions)
@@ -460,7 +421,6 @@ Server = _reflection.GeneratedProtocolMessageType('Server', (_message.Message,),
   ))
 _sym_db.RegisterMessage(Server)
 _sym_db.RegisterMessage(Server.TLSOptions)
-_sym_db.RegisterMessage(Server.TLSOptions.CertificateValidationContext)
 
 Port = _reflection.GeneratedProtocolMessageType('Port', (_message.Message,), dict(
   DESCRIPTOR = _PORT,
