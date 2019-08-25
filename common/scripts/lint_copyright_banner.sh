1f893e43d7291fc1e0d6387d5a3bc26d9f8bd668
Martin Taillefer <mtail@google.com>
2019-08-25 16:53:32 -0700

    Update common files.
    - Fix some broken dependency tracking.
    
    - Remove a superfluous proto import to get rid of a warning.
    
    - Explicitly select an older build-tools container version, since
    the current one has some problems building the protos.

--- common/scripts/lint_copyright_banner.sh
+++ common/scripts/lint_copyright_banner.sh
@@ -23,18 +23,17 @@
 
 set -e
 
-SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOTDIR=$(dirname "${SCRIPTPATH}")
-cd "${ROOTDIR}"
+ec=0
+for fn in "$@"; do
+  if ! grep -L -q -e "Apache License, Version 2" "${fn}"; then
+    echo "Missing license: ${fn}"
+    ec=1
+  fi
 
-if [[ "$1" == "--fix" ]]
-then
-    FIX="--fix"
-fi
+  if ! grep -L -q -e "Copyright" "${fn}"; then
+    echo "Missing copyright: ${fn}"
+    ec=1
+  fi
+done
 
-# if you want to update this version, also change the version number in .golangci.yml
-GOLANGCI_VERSION="v1.16.0"
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b "$GOPATH"/bin "$GOLANGCI_VERSION"
-golangci-lint --version
-# For tuning and when switching versions PLEASE REFERENCE: https://github.com/istio/istio/issues/14888
-env GOGC=25 golangci-lint run ${FIX} -j 8 -v ./...
+exit $ec
