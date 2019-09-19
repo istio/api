2ee7f433ccb15aa81408ac4d7a15383cc467693a
Kuat Yessenov <kuat@google.com>
2019-09-19 14:21:30 -0700

    fix envoy protos
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -340,7 +340,7 @@ envoy_pb_pythons := $(patsubst $(envoy_path)/%.proto,$(python_output_path)/$(env
 
 $(envoy_pb_gos): %.pb.go : %.proto
 	@$(protolock) status
-	@$(protoc) $(gogofast_plugin) $<
+	@$(protoc) --go_out=$(out_path) $<
 	@cp -r /tmp/istio.io/api/envoy/* envoy
 
 $(envoy_pb_pythons): $(envoy_protos)
