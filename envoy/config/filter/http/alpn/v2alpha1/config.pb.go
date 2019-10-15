cee616c6ce00eeba2e93ad2d5f4db7ab5a6e811d
crazyxy <yxyan@google.com>
2019-10-14 19:48:54 -0700

    update alpn filter api

--- envoy/config/filter/http/alpn/v2alpha1/config.pb.go
+++ envoy/config/filter/http/alpn/v2alpha1/config.pb.go
@@ -22,13 +22,44 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+// Upstream protocols
+type FilterConfig_Protocol int32
+
+const (
+	FilterConfig_HTTP10 FilterConfig_Protocol = 0
+	FilterConfig_HTTP11 FilterConfig_Protocol = 1
+	FilterConfig_HTTP2  FilterConfig_Protocol = 2
+)
+
+var FilterConfig_Protocol_name = map[int32]string{
+	0: "HTTP10",
+	1: "HTTP11",
+	2: "HTTP2",
+}
+
+var FilterConfig_Protocol_value = map[string]int32{
+	"HTTP10": 0,
+	"HTTP11": 1,
+	"HTTP2":  2,
+}
+
+func (x FilterConfig_Protocol) String() string {
+	return proto.EnumName(FilterConfig_Protocol_name, int32(x))
+}
+
+func (FilterConfig_Protocol) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_9dd199870dce382a, []int{0, 0}
+}
+
 // FilterConfig is the config for Istio-specific filter.
 type FilterConfig struct {
 	// A list of ALPN that will override the ALPN for upstream TLS connections.
-	AlpnOverride         []string `protobuf:"bytes,1,rep,name=alpn_override,json=alpnOverride,proto3" json:"alpn_override,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	AlpnOverride []string `protobuf:"bytes,1,rep,name=alpn_override,json=alpnOverride,proto3" json:"alpn_override,omitempty"` // Deprecated: Do not use.
+	// Map from upstream protocol to list of ALPN
+	AlpnOverridePerProtocol []*FilterConfig_AlpnOverride `protobuf:"bytes,2,rep,name=alpn_override_per_protocol,json=alpnOverridePerProtocol,proto3" json:"alpn_override_per_protocol,omitempty"`
+	XXX_NoUnkeyedLiteral    struct{}                     `json:"-"`
+	XXX_unrecognized        []byte                       `json:"-"`
+	XXX_sizecache           int32                        `json:"-"`
 }
 
 func (m *FilterConfig) Reset()         { *m = FilterConfig{} }
@@ -64,6 +95,7 @@ func (m *FilterConfig) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_FilterConfig proto.InternalMessageInfo
 
+// Deprecated: Do not use.
 func (m *FilterConfig) GetAlpnOverride() []string {
 	if m != nil {
 		return m.AlpnOverride
@@ -71,8 +103,74 @@ func (m *FilterConfig) GetAlpnOverride() []string {
 	return nil
 }
 
+func (m *FilterConfig) GetAlpnOverridePerProtocol() []*FilterConfig_AlpnOverride {
+	if m != nil {
+		return m.AlpnOverridePerProtocol
+	}
+	return nil
+}
+
+type FilterConfig_AlpnOverride struct {
+	// Upstream protocol
+	UpstreamProtocol FilterConfig_Protocol `protobuf:"varint,1,opt,name=upstream_protocol,json=upstreamProtocol,proto3,enum=istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig_Protocol" json:"upstream_protocol,omitempty"`
+	// A list of ALPN that will override the ALPN for upstream TLS connections.
+	AlpnOverrides        []string `protobuf:"bytes,2,rep,name=alpn_overrides,json=alpnOverrides,proto3" json:"alpn_overrides,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *FilterConfig_AlpnOverride) Reset()         { *m = FilterConfig_AlpnOverride{} }
+func (m *FilterConfig_AlpnOverride) String() string { return proto.CompactTextString(m) }
+func (*FilterConfig_AlpnOverride) ProtoMessage()    {}
+func (*FilterConfig_AlpnOverride) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9dd199870dce382a, []int{0, 0}
+}
+func (m *FilterConfig_AlpnOverride) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *FilterConfig_AlpnOverride) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_FilterConfig_AlpnOverride.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *FilterConfig_AlpnOverride) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_FilterConfig_AlpnOverride.Merge(m, src)
+}
+func (m *FilterConfig_AlpnOverride) XXX_Size() int {
+	return m.Size()
+}
+func (m *FilterConfig_AlpnOverride) XXX_DiscardUnknown() {
+	xxx_messageInfo_FilterConfig_AlpnOverride.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_FilterConfig_AlpnOverride proto.InternalMessageInfo
+
+func (m *FilterConfig_AlpnOverride) GetUpstreamProtocol() FilterConfig_Protocol {
+	if m != nil {
+		return m.UpstreamProtocol
+	}
+	return FilterConfig_HTTP10
+}
+
+func (m *FilterConfig_AlpnOverride) GetAlpnOverrides() []string {
+	if m != nil {
+		return m.AlpnOverrides
+	}
+	return nil
+}
+
 func init() {
+	proto.RegisterEnum("istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig_Protocol", FilterConfig_Protocol_name, FilterConfig_Protocol_value)
 	proto.RegisterType((*FilterConfig)(nil), "istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig")
+	proto.RegisterType((*FilterConfig_AlpnOverride)(nil), "istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.AlpnOverride")
 }
 
 func init() {
@@ -80,18 +178,27 @@ func init() {
 }
 
 var fileDescriptor_9dd199870dce382a = []byte{
-	// 167 bytes of a gzipped FileDescriptorProto
+	// 306 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x32, 0x4e, 0xcd, 0x2b, 0xcb,
 	0xaf, 0xd4, 0x4f, 0xce, 0xcf, 0x4b, 0xcb, 0x4c, 0xd7, 0x4f, 0xcb, 0xcc, 0x29, 0x49, 0x2d, 0xd2,
 	0xcf, 0x28, 0x29, 0x29, 0xd0, 0x4f, 0xcc, 0x29, 0xc8, 0xd3, 0x2f, 0x33, 0x4a, 0xcc, 0x29, 0xc8,
 	0x48, 0x34, 0x84, 0x2a, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xd2, 0xc9, 0x2c, 0x2e, 0xc9,
 	0xcc, 0xd7, 0x03, 0x6b, 0xd5, 0x83, 0xca, 0x40, 0xb4, 0xea, 0x81, 0xb4, 0xea, 0x81, 0xb4, 0xea,
-	0xc1, 0xb4, 0x2a, 0x19, 0x73, 0xf1, 0xb8, 0x81, 0x25, 0x9d, 0xc1, 0x2a, 0x85, 0x94, 0xb9, 0x78,
-	0x41, 0x0a, 0xe2, 0xf3, 0xcb, 0x52, 0x8b, 0x8a, 0x32, 0x53, 0x52, 0x25, 0x18, 0x15, 0x98, 0x35,
-	0x38, 0x83, 0x78, 0x40, 0x82, 0xfe, 0x50, 0x31, 0x27, 0xc7, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c,
-	0x92, 0x63, 0x7c, 0xf0, 0x48, 0x8e, 0x31, 0xca, 0x18, 0x62, 0x61, 0x66, 0xbe, 0x7e, 0x62, 0x41,
-	0xa6, 0x3e, 0x71, 0x4e, 0x4e, 0x62, 0x03, 0x3b, 0xd6, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x32,
-	0x45, 0x79, 0x7f, 0xe3, 0x00, 0x00, 0x00,
+	0xc1, 0xb4, 0x2a, 0x4d, 0x67, 0xe6, 0xe2, 0x71, 0x03, 0xcb, 0x3a, 0x83, 0x95, 0x0a, 0xa9, 0x73,
+	0xf1, 0x82, 0x54, 0xc4, 0xe7, 0x97, 0xa5, 0x16, 0x15, 0x65, 0xa6, 0xa4, 0x4a, 0x30, 0x2a, 0x30,
+	0x6b, 0x70, 0x3a, 0x31, 0x49, 0x30, 0x06, 0xf1, 0x80, 0x24, 0xfc, 0xa1, 0xe2, 0x42, 0x2d, 0x8c,
+	0x5c, 0x52, 0x28, 0x2a, 0xe3, 0x0b, 0x52, 0x8b, 0xe2, 0xc1, 0x6e, 0x48, 0xce, 0xcf, 0x91, 0x60,
+	0x52, 0x60, 0xd6, 0xe0, 0x36, 0x72, 0xd7, 0x23, 0xc5, 0x35, 0x7a, 0xc8, 0x2e, 0xd1, 0x73, 0x44,
+	0xb2, 0x2d, 0x48, 0x1c, 0xd9, 0xee, 0x80, 0xd4, 0xa2, 0x00, 0xa8, 0x3d, 0x52, 0xcb, 0x19, 0xb9,
+	0x78, 0x90, 0x55, 0x0a, 0x15, 0x70, 0x09, 0x96, 0x16, 0x14, 0x97, 0x14, 0xa5, 0x26, 0xe6, 0x22,
+	0x5c, 0xc3, 0xa8, 0xc0, 0xa8, 0xc1, 0x67, 0xe4, 0x4c, 0x81, 0x6b, 0x60, 0x16, 0x06, 0x09, 0xc0,
+	0x4c, 0x87, 0x89, 0x08, 0xa9, 0x72, 0xf1, 0xa1, 0x04, 0x44, 0x31, 0xd8, 0xf3, 0x9c, 0x41, 0xbc,
+	0xc8, 0x6e, 0x2e, 0x56, 0xd2, 0xe5, 0xe2, 0x80, 0x6b, 0xe1, 0xe2, 0x62, 0xf3, 0x08, 0x09, 0x09,
+	0x30, 0x34, 0x10, 0x60, 0x80, 0xb3, 0x0d, 0x05, 0x18, 0x85, 0x38, 0xb9, 0x58, 0x41, 0x6c, 0x23,
+	0x01, 0x26, 0x27, 0xc7, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48, 0x8e,
+	0x31, 0xca, 0x18, 0xe2, 0xf2, 0xcc, 0x7c, 0xfd, 0xc4, 0x82, 0x4c, 0x7d, 0xe2, 0xd2, 0x45, 0x12,
+	0x1b, 0xd8, 0xff, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x26, 0x8b, 0x55, 0xe2, 0x48, 0x02,
+	0x00, 0x00,
 }
 
 func (m *FilterConfig) Marshal() (dAtA []byte, err error) {
@@ -118,6 +225,20 @@ func (m *FilterConfig) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.XXX_unrecognized)
 		copy(dAtA[i:], m.XXX_unrecognized)
 	}
+	if len(m.AlpnOverridePerProtocol) > 0 {
+		for iNdEx := len(m.AlpnOverridePerProtocol) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.AlpnOverridePerProtocol[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintConfig(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x12
+		}
+	}
 	if len(m.AlpnOverride) > 0 {
 		for iNdEx := len(m.AlpnOverride) - 1; iNdEx >= 0; iNdEx-- {
 			i -= len(m.AlpnOverride[iNdEx])
@@ -126,6 +247,47 @@ func (m *FilterConfig) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 			i--
 			dAtA[i] = 0xa
 		}
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *FilterConfig_AlpnOverride) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *FilterConfig_AlpnOverride) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *FilterConfig_AlpnOverride) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if len(m.AlpnOverrides) > 0 {
+		for iNdEx := len(m.AlpnOverrides) - 1; iNdEx >= 0; iNdEx-- {
+			i -= len(m.AlpnOverrides[iNdEx])
+			copy(dAtA[i:], m.AlpnOverrides[iNdEx])
+			i = encodeVarintConfig(dAtA, i, uint64(len(m.AlpnOverrides[iNdEx])))
+			i--
+			dAtA[i] = 0x12
+		}
+	}
+	if m.UpstreamProtocol != 0 {
+		i = encodeVarintConfig(dAtA, i, uint64(m.UpstreamProtocol))
+		i--
+		dAtA[i] = 0x8
 	}
 	return len(dAtA) - i, nil
 }
@@ -149,6 +311,33 @@ func (m *FilterConfig) Size() (n int) {
 	_ = l
 	if len(m.AlpnOverride) > 0 {
 		for _, s := range m.AlpnOverride {
+			l = len(s)
+			n += 1 + l + sovConfig(uint64(l))
+		}
+	}
+	if len(m.AlpnOverridePerProtocol) > 0 {
+		for _, e := range m.AlpnOverridePerProtocol {
+			l = e.Size()
+			n += 1 + l + sovConfig(uint64(l))
+		}
+	}
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *FilterConfig_AlpnOverride) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.UpstreamProtocol != 0 {
+		n += 1 + sovConfig(uint64(m.UpstreamProtocol))
+	}
+	if len(m.AlpnOverrides) > 0 {
+		for _, s := range m.AlpnOverrides {
 			l = len(s)
 			n += 1 + l + sovConfig(uint64(l))
 		}
@@ -225,6 +414,145 @@ func (m *FilterConfig) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.AlpnOverride = append(m.AlpnOverride, string(dAtA[iNdEx:postIndex]))
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field AlpnOverridePerProtocol", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowConfig
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthConfig
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthConfig
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.AlpnOverridePerProtocol = append(m.AlpnOverridePerProtocol, &FilterConfig_AlpnOverride{})
+			if err := m.AlpnOverridePerProtocol[len(m.AlpnOverridePerProtocol)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipConfig(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthConfig
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthConfig
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *FilterConfig_AlpnOverride) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowConfig
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: AlpnOverride: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: AlpnOverride: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field UpstreamProtocol", wireType)
+			}
+			m.UpstreamProtocol = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowConfig
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.UpstreamProtocol |= FilterConfig_Protocol(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field AlpnOverrides", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowConfig
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthConfig
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthConfig
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.AlpnOverrides = append(m.AlpnOverrides, string(dAtA[iNdEx:postIndex]))
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
--- python/istio_api/envoy/config/filter/http/alpn/v2alpha1/config_pb2.py
+++ python/istio_api/envoy/config/filter/http/alpn/v2alpha1/config_pb2.py
@@ -20,22 +20,55 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='istio.envoy.config.filter.http.alpn.v2alpha1',
   syntax='proto3',
   serialized_options=_b('Z3istio.io/api/envoy/config/filter/http/alpn/v2alpha1'),
-  serialized_pb=_b('\n3envoy/config/filter/http/alpn/v2alpha1/config.proto\x12,istio.envoy.config.filter.http.alpn.v2alpha1\"%\n\x0c\x46ilterConfig\x12\x15\n\ralpn_override\x18\x01 \x03(\tB5Z3istio.io/api/envoy/config/filter/http/alpn/v2alpha1b\x06proto3')
+  serialized_pb=_b('\n3envoy/config/filter/http/alpn/v2alpha1/config.proto\x12,istio.envoy.config.filter.http.alpn.v2alpha1\"\xce\x02\n\x0c\x46ilterConfig\x12\x19\n\ralpn_override\x18\x01 \x03(\tB\x02\x18\x01\x12k\n\x1a\x61lpn_override_per_protocol\x18\x02 \x03(\x0b\x32G.istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.AlpnOverride\x1a\x86\x01\n\x0c\x41lpnOverride\x12^\n\x11upstream_protocol\x18\x01 \x01(\x0e\x32\x43.istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.Protocol\x12\x16\n\x0e\x61lpn_overrides\x18\x02 \x03(\t\"-\n\x08Protocol\x12\n\n\x06HTTP10\x10\x00\x12\n\n\x06HTTP11\x10\x01\x12\t\n\x05HTTP2\x10\x02\x42\x35Z3istio.io/api/envoy/config/filter/http/alpn/v2alpha1b\x06proto3')
 )
 
 
 
+_FILTERCONFIG_PROTOCOL = _descriptor.EnumDescriptor(
+  name='Protocol',
+  full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.Protocol',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='HTTP10', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='HTTP11', index=1, number=1,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='HTTP2', index=2, number=2,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=391,
+  serialized_end=436,
+)
+_sym_db.RegisterEnumDescriptor(_FILTERCONFIG_PROTOCOL)
 
-_FILTERCONFIG = _descriptor.Descriptor(
-  name='FilterConfig',
-  full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig',
+
+_FILTERCONFIG_ALPNOVERRIDE = _descriptor.Descriptor(
+  name='AlpnOverride',
+  full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.AlpnOverride',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='alpn_override', full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.alpn_override', index=0,
-      number=1, type=9, cpp_type=9, label=3,
+      name='upstream_protocol', full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.AlpnOverride.upstream_protocol', index=0,
+      number=1, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='alpn_overrides', full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.AlpnOverride.alpn_overrides', index=1,
+      number=2, type=9, cpp_type=9, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -52,20 +85,71 @@ _FILTERCONFIG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=101,
-  serialized_end=138,
+  serialized_start=255,
+  serialized_end=389,
 )
 
+_FILTERCONFIG = _descriptor.Descriptor(
+  name='FilterConfig',
+  full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='alpn_override', full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.alpn_override', index=0,
+      number=1, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=_b('\030\001'), file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='alpn_override_per_protocol', full_name='istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.alpn_override_per_protocol', index=1,
+      number=2, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[_FILTERCONFIG_ALPNOVERRIDE, ],
+  enum_types=[
+    _FILTERCONFIG_PROTOCOL,
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=102,
+  serialized_end=436,
+)
+
+_FILTERCONFIG_ALPNOVERRIDE.fields_by_name['upstream_protocol'].enum_type = _FILTERCONFIG_PROTOCOL
+_FILTERCONFIG_ALPNOVERRIDE.containing_type = _FILTERCONFIG
+_FILTERCONFIG.fields_by_name['alpn_override_per_protocol'].message_type = _FILTERCONFIG_ALPNOVERRIDE
+_FILTERCONFIG_PROTOCOL.containing_type = _FILTERCONFIG
 DESCRIPTOR.message_types_by_name['FilterConfig'] = _FILTERCONFIG
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 FilterConfig = _reflection.GeneratedProtocolMessageType('FilterConfig', (_message.Message,), {
+
+  'AlpnOverride' : _reflection.GeneratedProtocolMessageType('AlpnOverride', (_message.Message,), {
+    'DESCRIPTOR' : _FILTERCONFIG_ALPNOVERRIDE,
+    '__module__' : 'envoy.config.filter.http.alpn.v2alpha1.config_pb2'
+    # @@protoc_insertion_point(class_scope:istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig.AlpnOverride)
+    })
+  ,
   'DESCRIPTOR' : _FILTERCONFIG,
   '__module__' : 'envoy.config.filter.http.alpn.v2alpha1.config_pb2'
   # @@protoc_insertion_point(class_scope:istio.envoy.config.filter.http.alpn.v2alpha1.FilterConfig)
   })
 _sym_db.RegisterMessage(FilterConfig)
+_sym_db.RegisterMessage(FilterConfig.AlpnOverride)
 
 
 DESCRIPTOR._options = None
+_FILTERCONFIG.fields_by_name['alpn_override']._options = None
 # @@protoc_insertion_point(module_scope)
