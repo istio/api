52acc6489a5546a7b4172d6f69f5cce4e1ee8f17
Istio Automation <istio-testing-bot@google.com>
2020-05-07 09:46:27 -0700

    [release-1.6] Remove preserveUnknownFields from EnvoyFilter (#1424)
    * Remove preserveUnknownFields from EnvoyFilter
    
    * Remove preserveUnknownFields from instance and handler CRDs
    
    Co-authored-by: Jason Wang <jasonwzm@gmail.com>

--- networking/v1alpha3/envoy_filter.pb.go
+++ networking/v1alpha3/envoy_filter.pb.go
@@ -426,7 +426,6 @@ func (EnvoyFilter_Patch_Operation) EnumDescriptor() ([]byte, []int) {
 // +cue-gen:EnvoyFilter:subresource:status
 // +cue-gen:EnvoyFilter:scope:Namespaced
 // +cue-gen:EnvoyFilter:resource:categories=istio-io,networking-istio-io
-// +cue-gen:EnvoyFilter:preserveUnknownFields:false
 // -->
 //
 // <!-- go code generation tags
