69270ec082c79d6b8b9fcb2d5f0f7d3804dc1866
Abdel Sghiouar <abdelfettah@google.com>
2020-07-13 17:20:07 +0200

    fix gateways typo

--- networking/v1alpha3/envoy_filter.pb.go
+++ networking/v1alpha3/envoy_filter.pb.go
@@ -38,7 +38,7 @@
 // protocol filter on all sidecars in the system, for outbound port
 // 9307. The filter should be added before the terminating tcp_proxy
 // filter to take effect. In addition, it sets a 30s idle timeout for
-// all HTTP connections in both gateays and sidecars.
+// all HTTP connections in both gateways and sidecars.
 //
 // ```yaml
 // apiVersion: networking.istio.io/v1alpha3
