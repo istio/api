8d9af9be812c305e695ceadb853ae86116ffd6a6
Nikolay Pshenichnyy <nikolay.pshenichny@gmail.com>
2020-08-19 12:22:09 -0700

    revert removal of the export statement

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -59,7 +59,7 @@ fi
 
 # Build image to use
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-2020-08-19T17-29-34
+  export IMAGE_VERSION=master-2020-08-19T17-29-34
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   export IMAGE_NAME=build-tools
