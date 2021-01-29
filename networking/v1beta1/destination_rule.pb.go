e4de14b8fad42fada99be511c3bf196db5e575ef
John Howard <howardjohn@google.com>
2021-01-29 08:08:39 -0800

    Stop using hidden field in example
    Fixes https://github.com/istio/api/issues/909

--- networking/v1beta1/destination_rule.pb.go
+++ networking/v1beta1/destination_rule.pb.go
@@ -1510,7 +1510,7 @@ func (m *ConnectionPoolSettings_HTTPSettings) GetUseClientProtocol() bool {
 //         http2MaxRequests: 1000
 //         maxRequestsPerConnection: 10
 //     outlierDetection:
-//       consecutiveErrors: 7
+//       consecutive5xxErrors: 7
 //       interval: 5m
 //       baseEjectionTime: 15m
 // ```
@@ -1532,7 +1532,7 @@ func (m *ConnectionPoolSettings_HTTPSettings) GetUseClientProtocol() bool {
 //         http2MaxRequests: 1000
 //         maxRequestsPerConnection: 10
 //     outlierDetection:
-//       consecutiveErrors: 7
+//       consecutive5xxErrors: 7
 //       interval: 5m
 //       baseEjectionTime: 15m
 // ```
