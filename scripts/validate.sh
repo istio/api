9b9af17d07520efaa67d88fae181fe7a90e20b92
John Howard <howardjohn@google.com>
2021-02-16 19:28:32 -0800

    lint

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# shellcheck disable=SC2045,SC2016
 # Copyright Istio Authors
 
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,13 +21,13 @@ shopt -s globstar
 # for istioctl, which would prevent adding new fields (since they would fail the validation command).
 
 # Check if all examples are valid
-for proto in $(ls **/*.proto | grep -v ^common-protos | grep -v ^envoy); do
+for proto in $(find . -name "*.proto" | grep -v ^./common-protos | grep -v ^./envoy); do
     echo "Checking ${proto}..."
     # For each proto, we will remove comments (`//` or `// ` at start of line),
     # then find texts between ```yaml and ```. We separate each by ---, strip any ... (some docs elide resources),
     # then pass to istioctl validate
-    cat "${proto}" \
-    | sed 's/^\/\/ //g' | sed 's/^\/\///g' \
+    < "${proto}" \
+      sed 's/^\/\/ //g' | sed 's/^\/\///g' \
     | sed -n '/^```yaml/,/^```/ p' | sed 's/^```yaml/\-\-\-/g' \
     | grep -v '```' | grep -v '\.\.\.' \
     | istioctl validate -f -
@@ -37,6 +37,6 @@ done
 # Note: once we migrate to v1 CRDs we can move to scripts/validate_crds.py
 # Note: this misses package wide documentation
 for file in $(ls networking/v1beta1/*.gen.json); do
-    echo "Comparing $(basename $file)"
-    diff <(cat $file | jq --sort-keys) <(cat networking/v1alpha3/$(basename $file) | sed 's/v1alpha3/v1beta1/g' | jq --sort-keys)
+    echo "Comparing $(basename "${file}")"
+    diff <(< "${file}" jq --sort-keys) <(< "networking/v1alpha3/$(basename "${file}")" sed 's/v1alpha3/v1beta1/g' | jq --sort-keys)
 done
