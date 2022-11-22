bd9c37f95e1df61911b95437510f80d34ebf829f
Aryan Gupta <garyan@google.com>
2022-11-22 12:03:23 -0800

    security policy graduation to v1 (#2553)

--- security/v1/request_authentication.pb.go
+++ security/v1/request_authentication.pb.go
@@ -16,15 +16,15 @@
 // versions:
 // 	protoc-gen-go v1.28.1
 // 	protoc        (unknown)
-// source: security/v1beta1/request_authentication.proto
+// source: security/v1/request_authentication.proto
 
-// $schema: istio.security.v1beta1.RequestAuthentication
+// $schema: istio.security.v1.RequestAuthentication
 // $title: RequestAuthentication
 // $description: Request authentication configuration for workloads.
 // $location: https://istio.io/docs/reference/config/security/request_authentication.html
-// $aliases: [/docs/reference/config/security/v1beta1/request_authentication]
+// $aliases: [/docs/reference/config/security/v1/request_authentication]
 
-package v1beta1
+package v1
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
@@ -51,7 +51,7 @@ const (
 // - Require JWT for all request for workloads that have label `app:httpbin`
 //
 // ```yaml
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: RequestAuthentication
 // metadata:
 //
@@ -68,7 +68,7 @@ const (
 //	  jwksUri: https://example.com/.well-known/jwks.json
 //
 // ---
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: AuthorizationPolicy
 // metadata:
 //
@@ -92,7 +92,7 @@ const (
 // valid JWT token.
 //
 // ```yaml
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: RequestAuthentication
 // metadata:
 //
@@ -106,7 +106,7 @@ const (
 //	  jwksUri: https://example.com/.well-known/jwks.json
 //
 // ---
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: AuthorizationPolicy
 // metadata:
 //
@@ -127,7 +127,7 @@ const (
 // set from the OpenID Connect spec).
 //
 // ```yaml
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: RequestAuthentication
 // metadata:
 //
@@ -144,7 +144,7 @@ const (
 //	- issuer: "issuer-bar"
 //
 // ---
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: AuthorizationPolicy
 // metadata:
 //
@@ -177,7 +177,7 @@ const (
 // authorization policy could be:
 //
 // ```yaml
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: AuthorizationPolicy
 // metadata:
 //
@@ -213,7 +213,7 @@ const (
 // - VirtualService to route the request based on the "sub" claim.
 //
 // ```yaml
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: RequestAuthentication
 // metadata:
 //
@@ -230,7 +230,7 @@ const (
 //	  jwksUri: https://example.com/.well-known/jwks.json
 //
 // ---
-// apiVersion: security.istio.io/v1beta1
+// apiVersion: security.istio.io/v1
 // kind: AuthorizationPolicy
 // metadata:
 //
@@ -280,8 +280,7 @@ const (
 //
 // <!-- crd generation tags
 // +cue-gen:RequestAuthentication:groupName:security.istio.io
-// +cue-gen:RequestAuthentication:version:v1beta1
-// +cue-gen:RequestAuthentication:storageVersion
+// +cue-gen:RequestAuthentication:version:v1
 // +cue-gen:RequestAuthentication:annotations:helm.sh/resource-policy=keep
 // +cue-gen:RequestAuthentication:labels:app=istio-pilot,chart=istio,istio=security,heritage=Tiller,release=istio
 // +cue-gen:RequestAuthentication:subresource:status
@@ -292,12 +291,12 @@ const (
 //
 // <!-- go code generation tags
 // +kubetype-gen
-// +kubetype-gen:groupVersion=security.istio.io/v1beta1
+// +kubetype-gen:groupVersion=security.istio.io/v1
 // +genclient
 // +k8s:deepcopy-gen=true
 // -->
 // <!-- istio code generation tags
-// +istio.io/sync-start
+// +istio.io/sync-from:security/v1beta1/request_authentication.proto
 // -->
 type RequestAuthentication struct {
 	state         protoimpl.MessageState
@@ -323,7 +322,7 @@ type RequestAuthentication struct {
 func (x *RequestAuthentication) Reset() {
 	*x = RequestAuthentication{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_security_v1beta1_request_authentication_proto_msgTypes[0]
+		mi := &file_security_v1_request_authentication_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -336,7 +335,7 @@ func (x *RequestAuthentication) String() string {
 func (*RequestAuthentication) ProtoMessage() {}
 
 func (x *RequestAuthentication) ProtoReflect() protoreflect.Message {
-	mi := &file_security_v1beta1_request_authentication_proto_msgTypes[0]
+	mi := &file_security_v1_request_authentication_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -349,7 +348,7 @@ func (x *RequestAuthentication) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use RequestAuthentication.ProtoReflect.Descriptor instead.
 func (*RequestAuthentication) Descriptor() ([]byte, []int) {
-	return file_security_v1beta1_request_authentication_proto_rawDescGZIP(), []int{0}
+	return file_security_v1_request_authentication_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *RequestAuthentication) GetSelector() *v1beta1.WorkloadSelector {
@@ -366,53 +365,51 @@ func (x *RequestAuthentication) GetJwtRules() []*JWTRule {
 	return nil
 }
 
-var File_security_v1beta1_request_authentication_proto protoreflect.FileDescriptor
+var File_security_v1_request_authentication_proto protoreflect.FileDescriptor
 
-var file_security_v1beta1_request_authentication_proto_rawDesc = []byte{
-	0x0a, 0x2d, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2f, 0x76, 0x31, 0x62, 0x65, 0x74,
-	0x61, 0x31, 0x2f, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x5f, 0x61, 0x75, 0x74, 0x68, 0x65,
-	0x6e, 0x74, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
-	0x16, 0x69, 0x73, 0x74, 0x69, 0x6f, 0x2e, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2e,
-	0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x1a, 0x1b, 0x74, 0x79, 0x70, 0x65, 0x2f, 0x76, 0x31,
-	0x62, 0x65, 0x74, 0x61, 0x31, 0x2f, 0x73, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x2e, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x1a, 0x1a, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2f, 0x76,
-	0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x2f, 0x6a, 0x77, 0x74, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x22, 0x97, 0x01, 0x0a, 0x15, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x41, 0x75, 0x74, 0x68,
-	0x65, 0x6e, 0x74, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x40, 0x0a, 0x08, 0x73, 0x65,
-	0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x24, 0x2e, 0x69,
-	0x73, 0x74, 0x69, 0x6f, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x2e, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61,
-	0x31, 0x2e, 0x57, 0x6f, 0x72, 0x6b, 0x6c, 0x6f, 0x61, 0x64, 0x53, 0x65, 0x6c, 0x65, 0x63, 0x74,
-	0x6f, 0x72, 0x52, 0x08, 0x73, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x12, 0x3c, 0x0a, 0x09,
-	0x6a, 0x77, 0x74, 0x5f, 0x72, 0x75, 0x6c, 0x65, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0b, 0x32,
-	0x1f, 0x2e, 0x69, 0x73, 0x74, 0x69, 0x6f, 0x2e, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79,
-	0x2e, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x2e, 0x4a, 0x57, 0x54, 0x52, 0x75, 0x6c, 0x65,
-	0x52, 0x08, 0x6a, 0x77, 0x74, 0x52, 0x75, 0x6c, 0x65, 0x73, 0x42, 0x1f, 0x5a, 0x1d, 0x69, 0x73,
-	0x74, 0x69, 0x6f, 0x2e, 0x69, 0x6f, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x73, 0x65, 0x63, 0x75, 0x72,
-	0x69, 0x74, 0x79, 0x2f, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+var file_security_v1_request_authentication_proto_rawDesc = []byte{
+	0x0a, 0x28, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2f, 0x76, 0x31, 0x2f, 0x72, 0x65,
+	0x71, 0x75, 0x65, 0x73, 0x74, 0x5f, 0x61, 0x75, 0x74, 0x68, 0x65, 0x6e, 0x74, 0x69, 0x63, 0x61,
+	0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x11, 0x69, 0x73, 0x74, 0x69,
+	0x6f, 0x2e, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2e, 0x76, 0x31, 0x1a, 0x15, 0x73,
+	0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2f, 0x76, 0x31, 0x2f, 0x6a, 0x77, 0x74, 0x2e, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x1a, 0x1b, 0x74, 0x79, 0x70, 0x65, 0x2f, 0x76, 0x31, 0x62, 0x65, 0x74,
+	0x61, 0x31, 0x2f, 0x73, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x22, 0x92, 0x01, 0x0a, 0x15, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x41, 0x75, 0x74,
+	0x68, 0x65, 0x6e, 0x74, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x40, 0x0a, 0x08, 0x73,
+	0x65, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x24, 0x2e,
+	0x69, 0x73, 0x74, 0x69, 0x6f, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x2e, 0x76, 0x31, 0x62, 0x65, 0x74,
+	0x61, 0x31, 0x2e, 0x57, 0x6f, 0x72, 0x6b, 0x6c, 0x6f, 0x61, 0x64, 0x53, 0x65, 0x6c, 0x65, 0x63,
+	0x74, 0x6f, 0x72, 0x52, 0x08, 0x73, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x12, 0x37, 0x0a,
+	0x09, 0x6a, 0x77, 0x74, 0x5f, 0x72, 0x75, 0x6c, 0x65, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0b,
+	0x32, 0x1a, 0x2e, 0x69, 0x73, 0x74, 0x69, 0x6f, 0x2e, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74,
+	0x79, 0x2e, 0x76, 0x31, 0x2e, 0x4a, 0x57, 0x54, 0x52, 0x75, 0x6c, 0x65, 0x52, 0x08, 0x6a, 0x77,
+	0x74, 0x52, 0x75, 0x6c, 0x65, 0x73, 0x42, 0x1a, 0x5a, 0x18, 0x69, 0x73, 0x74, 0x69, 0x6f, 0x2e,
+	0x69, 0x6f, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x73, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2f,
+	0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_security_v1beta1_request_authentication_proto_rawDescOnce sync.Once
-	file_security_v1beta1_request_authentication_proto_rawDescData = file_security_v1beta1_request_authentication_proto_rawDesc
+	file_security_v1_request_authentication_proto_rawDescOnce sync.Once
+	file_security_v1_request_authentication_proto_rawDescData = file_security_v1_request_authentication_proto_rawDesc
 )
 
-func file_security_v1beta1_request_authentication_proto_rawDescGZIP() []byte {
-	file_security_v1beta1_request_authentication_proto_rawDescOnce.Do(func() {
-		file_security_v1beta1_request_authentication_proto_rawDescData = protoimpl.X.CompressGZIP(file_security_v1beta1_request_authentication_proto_rawDescData)
+func file_security_v1_request_authentication_proto_rawDescGZIP() []byte {
+	file_security_v1_request_authentication_proto_rawDescOnce.Do(func() {
+		file_security_v1_request_authentication_proto_rawDescData = protoimpl.X.CompressGZIP(file_security_v1_request_authentication_proto_rawDescData)
 	})
-	return file_security_v1beta1_request_authentication_proto_rawDescData
+	return file_security_v1_request_authentication_proto_rawDescData
 }
 
-var file_security_v1beta1_request_authentication_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_security_v1beta1_request_authentication_proto_goTypes = []interface{}{
-	(*RequestAuthentication)(nil),    // 0: istio.security.v1beta1.RequestAuthentication
+var file_security_v1_request_authentication_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_security_v1_request_authentication_proto_goTypes = []interface{}{
+	(*RequestAuthentication)(nil),    // 0: istio.security.v1.RequestAuthentication
 	(*v1beta1.WorkloadSelector)(nil), // 1: istio.type.v1beta1.WorkloadSelector
-	(*JWTRule)(nil),                  // 2: istio.security.v1beta1.JWTRule
+	(*JWTRule)(nil),                  // 2: istio.security.v1.JWTRule
 }
-var file_security_v1beta1_request_authentication_proto_depIdxs = []int32{
-	1, // 0: istio.security.v1beta1.RequestAuthentication.selector:type_name -> istio.type.v1beta1.WorkloadSelector
-	2, // 1: istio.security.v1beta1.RequestAuthentication.jwt_rules:type_name -> istio.security.v1beta1.JWTRule
+var file_security_v1_request_authentication_proto_depIdxs = []int32{
+	1, // 0: istio.security.v1.RequestAuthentication.selector:type_name -> istio.type.v1beta1.WorkloadSelector
+	2, // 1: istio.security.v1.RequestAuthentication.jwt_rules:type_name -> istio.security.v1.JWTRule
 	2, // [2:2] is the sub-list for method output_type
 	2, // [2:2] is the sub-list for method input_type
 	2, // [2:2] is the sub-list for extension type_name
@@ -420,14 +417,14 @@ var file_security_v1beta1_request_authentication_proto_depIdxs = []int32{
 	0, // [0:2] is the sub-list for field type_name
 }
 
-func init() { file_security_v1beta1_request_authentication_proto_init() }
-func file_security_v1beta1_request_authentication_proto_init() {
-	if File_security_v1beta1_request_authentication_proto != nil {
+func init() { file_security_v1_request_authentication_proto_init() }
+func file_security_v1_request_authentication_proto_init() {
+	if File_security_v1_request_authentication_proto != nil {
 		return
 	}
-	file_security_v1beta1_jwt_proto_init()
+	file_security_v1_jwt_proto_init()
 	if !protoimpl.UnsafeEnabled {
-		file_security_v1beta1_request_authentication_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_security_v1_request_authentication_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*RequestAuthentication); i {
 			case 0:
 				return &v.state
@@ -444,18 +441,18 @@ func file_security_v1beta1_request_authentication_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_security_v1beta1_request_authentication_proto_rawDesc,
+			RawDescriptor: file_security_v1_request_authentication_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_security_v1beta1_request_authentication_proto_goTypes,
-		DependencyIndexes: file_security_v1beta1_request_authentication_proto_depIdxs,
-		MessageInfos:      file_security_v1beta1_request_authentication_proto_msgTypes,
+		GoTypes:           file_security_v1_request_authentication_proto_goTypes,
+		DependencyIndexes: file_security_v1_request_authentication_proto_depIdxs,
+		MessageInfos:      file_security_v1_request_authentication_proto_msgTypes,
 	}.Build()
-	File_security_v1beta1_request_authentication_proto = out.File
-	file_security_v1beta1_request_authentication_proto_rawDesc = nil
-	file_security_v1beta1_request_authentication_proto_goTypes = nil
-	file_security_v1beta1_request_authentication_proto_depIdxs = nil
+	File_security_v1_request_authentication_proto = out.File
+	file_security_v1_request_authentication_proto_rawDesc = nil
+	file_security_v1_request_authentication_proto_goTypes = nil
+	file_security_v1_request_authentication_proto_depIdxs = nil
 }
