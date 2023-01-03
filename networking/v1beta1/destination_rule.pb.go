abd31091d074a1875b5d7dfdf0ceb0f4041b26b7
Rama Chavali <rama.rao@salesforce.com>
2023-01-03 07:45:34 +0530

    clarify warmup duration docs in destination rule (#2612)
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- networking/v1beta1/destination_rule.pb.go
+++ networking/v1beta1/destination_rule.pb.go
@@ -884,6 +884,9 @@ type LoadBalancerSettings struct {
 	// remains in warmup mode starting from its creation time for the duration of this window and
 	// Istio progressively increases amount of traffic for that endpoint instead of sending proportional amount of traffic.
 	// This should be enabled for services that require warm up time to serve full production load with reasonable latency.
+	// Please note that this is most effective when few new endpoints come up like scale event in Kubernetes. When all the
+	// endpoints are relatively new like new deployment, this is not very effective as all endpoints end up getting same
+	// amount of requests.
 	// Currently this is only supported for ROUND_ROBIN and LEAST_REQUEST load balancers.
 	WarmupDurationSecs *duration.Duration `protobuf:"bytes,4,opt,name=warmup_duration_secs,json=warmupDurationSecs,proto3" json:"warmup_duration_secs,omitempty"`
 }
