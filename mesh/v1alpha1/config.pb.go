eccaf6cba0ac0f007ed99af071fdfd094d04c86d
Faseela K <faseela.k@est.tech>
2023-10-26 16:07:04 +0200

    Update tlsDefaults doc to include cipher suites support
    Signed-off-by: Faseela K <faseela.k@est.tech>

--- mesh/v1alpha1/config.pb.go
+++ mesh/v1alpha1/config.pb.go
@@ -871,7 +871,7 @@ type MeshConfig struct {
 	// Note: Mesh mTLS does not respect ECDH curves.
 	MeshMTLS *MeshConfig_TLSConfig `protobuf:"bytes,63,opt,name=mesh_mTLS,json=meshMTLS,proto3" json:"mesh_mTLS,omitempty"`
 	// Configuration of TLS for all traffic except for ISTIO_MUTUAL mode.
-	// Currently, this supports configuration of ecdh_curves only.
+	// Currently, this supports configuration of ecdh_curves and cipher_suites only.
 	// For ISTIO_MUTUAL TLS settings, use meshMTLS configuration.
 	TlsDefaults *MeshConfig_TLSConfig `protobuf:"bytes,64,opt,name=tls_defaults,json=tlsDefaults,proto3" json:"tls_defaults,omitempty"`
 }
