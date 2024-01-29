62e5dd9150621b4ba903faf6f1f5be80e12153b8
zirain <zirain2009@gmail.com>
2024-01-30 04:55:32 +0800

    add shortname for authz (#3069)
    * add shortname for authz
    
    * rename to ap
    
    * release notes

--- security/v1/authorization_policy.pb.go
+++ security/v1/authorization_policy.pb.go
@@ -556,7 +556,7 @@ func (AuthorizationPolicy_Action) EnumDescriptor() ([]byte, []int) {
 // +cue-gen:AuthorizationPolicy:labels:app=istio-pilot,chart=istio,istio=security,heritage=Tiller,release=istio
 // +cue-gen:AuthorizationPolicy:subresource:status
 // +cue-gen:AuthorizationPolicy:scope:Namespaced
-// +cue-gen:AuthorizationPolicy:resource:categories=istio-io,security-istio-io,plural=authorizationpolicies
+// +cue-gen:AuthorizationPolicy:resource:categories=istio-io,security-istio-io,shortNames=ap,plural=authorizationpolicies
 // +cue-gen:AuthorizationPolicy:preserveUnknownFields:false
 // -->
 //
