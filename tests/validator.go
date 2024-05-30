ab3f336ce327aa8fa3709fffd455575f5a1d80c6
John Howard <john.howard@solo.io>
2024-05-30 07:41:33 -0700

    Fix CEL validation on older k8s versions
    Also add testing so we can catch this (though its tied to a single k8s
    version, so its not perfect -- but should be able to catch most cases).
    
    CEL before k8s 1.30 did not estimate the cost correctly so its basically
    impossible

--- tests/validator.go
+++ tests/validator.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextval "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/validation"
 	structuralschema "k8s.io/apiextensions-apiserver/pkg/apiserver/schema"
 	"k8s.io/apiextensions-apiserver/pkg/apiserver/schema/cel"
 	structuraldefaulting "k8s.io/apiextensions-apiserver/pkg/apiserver/schema/defaulting"
@@ -36,6 +37,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	kubeyaml "k8s.io/apimachinery/pkg/util/yaml"
 	celconfig "k8s.io/apiserver/pkg/apis/cel"
 )
@@ -176,6 +178,13 @@ func NewValidatorFromCRDs(crds ...apiextensions.CustomResourceDefinition) (*Vali
 		if len(versions) == 0 {
 			versions = []apiextensions.CustomResourceDefinitionVersion{{Name: crd.Spec.Version}} // nolint: staticcheck
 		}
+		for _, v := range versions {
+			crd.Status.StoredVersions = append(crd.Status.StoredVersions, v.Name)
+		}
+		errs := apiextval.ValidateCustomResourceDefinition(context.Background(), &crd)
+		if len(errs) > 0 {
+			return nil, fmt.Errorf("CRD %v is not valid: %v", crd.Name, formatError(errs))
+		}
 		for _, ver := range versions {
 			gvk := schema.GroupVersionKind{
 				Group:   crd.Spec.Group,
@@ -210,6 +219,18 @@ func NewValidatorFromCRDs(crds ...apiextensions.CustomResourceDefinition) (*Vali
 	}
 
 	return v, nil
+}
+
+func formatError(errs field.ErrorList) error {
+	if len(errs) == 0 {
+		return nil
+	}
+	s := strings.Builder{}
+	s.WriteString("\n")
+	for _, e := range errs.ToAggregate().Errors() {
+		s.WriteString(fmt.Sprintf("- %v\n", strings.TrimPrefix(e.Error(), "spec.validation.openAPIV3Schema")))
+	}
+	return fmt.Errorf(s.String())
 }
 
 func NewIstioValidator(t *testing.T) *Validator {
