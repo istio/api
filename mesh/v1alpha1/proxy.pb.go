54d959695768224f2543ece15b4e5e7bf16281b9
Leonardo Sarra <8293810+leosarra@users.noreply.github.com>
2024-09-24 22:13:25 +0200

    Clarify ForwardClientCertDetails comments (#3315)
    * Clarify ForwardClientCertDetails comments
    
    * update gen files
    
    * fix typo
    
    * remove mention of gateway proxy
    
    * fix default value for gw

--- mesh/v1alpha1/proxy.pb.go
+++ mesh/v1alpha1/proxy.pb.go
@@ -95,7 +95,7 @@ func (AuthenticationPolicy) EnumDescriptor() ([]byte, []int) {
 }
 
 // ForwardClientCertDetails controls how the x-forwarded-client-cert (XFCC)
-// header is handled by the gateway proxy.
+// header is handled by a proxy.
 // See [Envoy XFCC](https://www.envoyproxy.io/docs/envoy/latest/api-v3/extensions/filters/network/http_connection_manager/v3/http_connection_manager.proto.html#enum-extensions-filters-network-http-connection-manager-v3-httpconnectionmanager-forwardclientcertdetails)
 // header handling for more details.
 type ForwardClientCertDetails int32
@@ -103,16 +103,16 @@ type ForwardClientCertDetails int32
 const (
 	// Field is not set
 	ForwardClientCertDetails_UNDEFINED ForwardClientCertDetails = 0
-	// Do not send the XFCC header to the next hop. This is the default value.
+	// Do not send the XFCC header to the next hop.
 	ForwardClientCertDetails_SANITIZE ForwardClientCertDetails = 1
 	// When the client connection is mTLS (Mutual TLS), forward the XFCC header
 	// in the request.
 	ForwardClientCertDetails_FORWARD_ONLY ForwardClientCertDetails = 2
 	// When the client connection is mTLS, append the client certificate
-	// information to the request’s XFCC header and forward it.
+	// information to the request’s XFCC header and forward it. This is the default value for sidecar proxies.
 	ForwardClientCertDetails_APPEND_FORWARD ForwardClientCertDetails = 3
 	// When the client connection is mTLS, reset the XFCC header with the client
-	// certificate information and send it to the next hop.
+	// certificate information and send it to the next hop. This is the default value for gateway proxies.
 	ForwardClientCertDetails_SANITIZE_SET ForwardClientCertDetails = 4
 	// Always forward the XFCC header in the request, regardless of whether the
 	// client connection is mTLS.
