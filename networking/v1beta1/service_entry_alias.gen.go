7b2449c9a94a5d847daac57a1c316f0c39a6a01a
John Howard <john.howard@solo.io>
2024-11-18 08:42:31 -0800

    Fix incorrect SE docs
    There is no 'egress policy'.
    
    Fixes https://github.com/istio/istio/issues/53904

--- networking/v1beta1/service_entry_alias.gen.go
+++ networking/v1beta1/service_entry_alias.gen.go
@@ -43,9 +43,7 @@ type ServiceEntry = v1alpha3.ServiceEntry
 // Location specifies whether the service is part of Istio mesh or
 // outside the mesh.  Location determines the behavior of several
 // features, such as service-to-service mTLS authentication, policy
-// enforcement, etc. When communicating with services outside the mesh,
-// Istio's mTLS authentication is disabled, and policy enforcement is
-// performed on the client-side as opposed to server-side.
+// enforcement, etc.
 type ServiceEntry_Location = v1alpha3.ServiceEntry_Location
 
 // Signifies that the service is external to the mesh. Typically used
